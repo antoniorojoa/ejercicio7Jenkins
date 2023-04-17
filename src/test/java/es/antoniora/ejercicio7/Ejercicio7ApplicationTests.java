9a894c185f9cea13dac7740edfa0000e3ec928e9
antoniorojoa <114166598+antoniorojoa@users.noreply.github.com>
2023-04-17 21:11:39 +0200

    Add files via upload

---
+++ src/test/java/es/antoniora/ejercicio7/Ejercicio7ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.antoniora.ejercicio7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
